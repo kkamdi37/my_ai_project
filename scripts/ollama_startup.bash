6157d68f48321d204e187fde7b41889753f6ed40
kkamdi37 <kkamdi37@gmail.com>
2026-03-31 23:11:37 +0900

    Change 'p' flag to 'publish' in docker run command

--- scripts/ollama_startup.bash
+++ scripts/ollama_startup.bash
@@ -1,1 +1,1 @@
-sudo docker run -dit --rm --env OLLAMA_HOST=0.0.0.0 -p 11434:11434 --name my-ollama dooboo1818/myollama
+sudo docker run -dit --rm --env OLLAMA_HOST=0.0.0.0 --publish 11434:11434 --name my-ollama dooboo1818/myollama
